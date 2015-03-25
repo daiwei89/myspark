71da019e7535ab3b6fbff14a6f5389bfe1bd094a
daiwei89 <daiwei89@gmail.com>
2015-03-24 21:11:52 -0400

    confs

--- conf/spark-env.sh
+++ conf/spark-env.sh
@@ -14,7 +14,7 @@
 # - SPARK_LOCAL_IP, to set the IP address Spark binds to on this node
 eth1_ip=`ifconfig eth1 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
 dfg_ip=`ifconfig fge0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
-export "SPARK_LOCAL_IP=${eth1_ip}"
+export "SPARK_LOCAL_IP=${dfg_ip}"
 # - SPARK_PUBLIC_DNS, to set the public DNS name of the driver program
 # - SPARK_CLASSPATH, default classpath entries to append
 # - SPARK_LOCAL_DIRS, storage directories to use on this node for shuffle and RDD data
@@ -26,7 +26,7 @@ export "SPARK_LOCAL_IP=${eth1_ip}"
 # - SPARK_EXECUTOR_CORES, Number of cores for the workers (Default: 1).
 # - SPARK_EXECUTOR_MEMORY, Memory per Worker (e.g. 1000M, 2G) (Default: 1G)
 # - SPARK_DRIVER_MEMORY, Memory for Master (e.g. 1000M, 2G) (Default: 512 Mb)
-export "SPARK_DRIVER_MEMORY=60G"
+export "SPARK_DRIVER_MEMORY=100G"
 # - SPARK_YARN_APP_NAME, The name of your application (Default: Spark)
 # - SPARK_YARN_QUEUE, The hadoop queue to use for allocation requests (Default: ‘default’)
 # - SPARK_YARN_DIST_FILES, Comma separated list of files to be distributed with the job.
@@ -39,7 +39,7 @@ export "SPARK_MASTER_IP=10.53.1.1"
 # - SPARK_MASTER_OPTS, to set config properties only for the master (e.g. "-Dx=y")
 # - SPARK_WORKER_CORES, to set the number of cores to use on this machine
 export "SPARK_WORKER_CORES=64"
-# - SPARK_WORKER_MEMORY, to set how much total memory workers have to give executors (e.g. 1000m, 2g)
+export "SPARK_WORKER_MEMORY=125G" #, to set how much total memory workers have to give executors (e.g. 1000m, 2g)
 # - SPARK_WORKER_PORT / SPARK_WORKER_WEBUI_PORT, to use non-default ports for the worker
 # - SPARK_WORKER_INSTANCES, to set the number of worker processes per node
 # - SPARK_WORKER_DIR, to set the working directory of worker processes
